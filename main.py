ff7a9e3ed81c16f7813875171fc3600ae1d94774
Gabriel.Gontijo <38219914+GGontijo@users.noreply.github.com>
2023-01-14 16:25:14 -0400

    update

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 import subprocess
+from sys import platform
+import os
+import json
+from config_helper import Config
 
 
 class ScanRede:
     
     def __init__(self) -> None:
-        subprocess.call(['sh', './scan_rede.sh'])
+        config = Config()
+        self.config_parameters = config.get_config('parameters')
+        self.config_telegram = config.get_config('telegram')
+        self.config_shinobi = config.get_config('shinobi')
+        self.ler_status()
+        self.scanear_rede()
+
+    
+    def ler_status(self):
+        try:
+            with open('cachefile', 'r') as file:
+                self.current_status = json.load(file)
+        except:
+            self.default_status()
+            with open('cachefile', 'r') as file:
+                self.current_status = json.load(file)
+
+
+    def default_status(self):
+        status = {
+            "presence": [],
+            "status": True
+        }
+
+        json_file = json.dumps(status, indent=4)
+
+        with open('cachefile', 'w') as file:
+            file.write(json_file)
+
+
+    def scanear_rede(self):
+        if platform != 'linux':
+            return
+        
+        os.system()
+
+if __name__ == '__main__':
+    ScanRede()
